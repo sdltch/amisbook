05af0165b240eb9b5bb14b64ed13ada37472eb9d
毛豆 <596211206@qq.com>
2020-10-19 16:47:11 +0800

    23

---
+++ 接收程序-develop/customs-sync-worker.cmd
@@ -0,0 +1,3 @@
+chcp 65001
+
+java -Dfile.encoding=UTF-8 -cp .;amiintellect-customs-sync-worker-1.0.jar;lib/* com.amiintellect.customs.sync.worker.App
---
+++ 接收程序-develop/customs-sync-worker.sh
@@ -0,0 +1,1 @@
+java -cp .:amiintellect-customs-sync-worker-1.0.jar:lib/* com.amiintellect.customs.sync.worker.App &
